7b30fb03c05c0d78c0c56a542e6b3ca00ca9ce04
celeron1ghz <celeron1ghz@gmail.com>
2017-08-09 11:40:02 +0900

    impl redirect

--- handler.js
+++ handler.js
@@ -1,6 +1,18 @@
 'use strict';
 
+const aws    = require('aws-sdk');
+const s3     = new aws.S3({ region: 'ap-northeast-1' });
+const BUCKET = 'lambda-random-image';
+
 module.exports.main = (event, context, callback) => {
-  const response = { statusCode: 302, location: 'http://google.com/' };
-  callback(null, response);
+  s3.listObjects({ Bucket: BUCKET }).promise().then(data => {
+    const idx = Math.floor(Math.random() * data.Contents.length);
+    const file = data.Contents[idx];
+    console.log(data);
+
+    const url = `https://s3-ap-northeast-1.amazonaws.com/${BUCKET}/${file.Key}`;
+    console.log(url);
+    callback(null, { statusCode: 302, location: url });
+  })
+  .catch(e => console.log(e));
 };
