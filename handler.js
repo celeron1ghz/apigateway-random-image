6ab907098e426c7967c81487ec3f521ddcb2a232
celeron1ghz <celeron1ghz@gmail.com>
2017-08-10 09:21:43 +0900

    change to return image content

--- handler.js
+++ handler.js
@@ -8,11 +8,16 @@ module.exports.main = (event, context, callback) => {
   s3.listObjects({ Bucket: BUCKET }).promise().then(data => {
     const idx = Math.floor(Math.random() * data.Contents.length);
     const file = data.Contents[idx];
-    console.log(data);
-
-    const url = `https://s3-ap-northeast-1.amazonaws.com/${BUCKET}/${file.Key}`;
-    console.log(url);
-    callback(null, { statusCode: 302, location: url });
+    console.log(file.Key);
+    return s3.getObject({ Bucket: BUCKET, Key: file.Key }).promise();
+  })
+  .then(data => {
+    callback(null, {
+        statusCode: 200,
+        headers: { 'Content-Type': 'image/jpeg' },
+        body: data.Body.toString('base64'),
+        isBase64Encoded: true,
+    });
   })
   .catch(e => console.log(e));
 };
